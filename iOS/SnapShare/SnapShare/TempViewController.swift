2ae34cc315f4c315eb8da85ec6a972b697f5fef5
aakagi <aakagi@purdue.edu>
2016-02-12 15:08:59 -0800

    holy shit ios snapchat video works

--- iOS/SnapShare/SnapShare/TempViewController.swift
+++ iOS/SnapShare/SnapShare/TempViewController.swift
@@ -19,16 +19,45 @@ class TempViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let url = NSURL(string: "https://s3.amazonaws.com/kargopolov/kukushka.mp3"))
+//        let url = NSURL(string: "https://s3.amazonaws.com/kargopolov/kukushka.mp3")
+        
+        let url = NSURL(string: "http://www.akagi.co/video/snapstory.m4v")
+        
         playerItem = AVPlayerItem(URL: url!)
         player = AVPlayer(playerItem: playerItem!)
         
-        let playerLayer
+        let playerLayer = AVPlayerLayer(player: player!)
+        playerLayer.frame = CGRectMake(0,50,self.view.frame.size.width,200)
+        self.view.layer.addSublayer(playerLayer)
         
-        
-        
+        playToggle.addTarget(self, action: "playButtonTapped:", forControlEvents: .TouchUpInside)
     }
     
+    override func viewWillAppear(animated: Bool) {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "finishedPlaying:", name: AVPlayerItemDidPlayToEndTimeNotification, object: playerItem)
+    }
     
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func playButtonTapped(sender: AnyObject) {
+        if player?.rate == 0 {
+            player!.play()
+            playToggle.setTitle("Pause", forState: UIControlState.Normal)
+        }
+        else {
+            player!.pause()
+            playToggle.setTitle("Play", forState: UIControlState.Normal)
+        }
+    }
+    
+    func finishedPlaying(notification: NSNotification) {
+        playToggle.setTitle("Play", forState: UIControlState.Normal)
+        let endOfStream: AVPlayerItem = notification.object as! AVPlayerItem
+        endOfStream.seekToTime(kCMTimeZero)
+        
+    }
+
     
 }
