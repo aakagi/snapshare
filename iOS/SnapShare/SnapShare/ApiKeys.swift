24ff153f7b52516c682a5129035a9f01503847c9
aakagi <aakagi@purdue.edu>
2016-02-15 11:49:50 -0800

    testing hiding api keys in swift from git

---
+++ iOS/SnapShare/SnapShare/ApiKeys.swift
@@ -0,0 +1,13 @@
+//
+//  ApiKeys.swift
+//  SnapShare
+//
+//  Created by Alexander Akagi on 2/15/16.
+//  Copyright © 2016 Alexander Akagi. All rights reserved.
+//
+
+import Foundation
+
+struct ApiKeys {
+    let test = "testinggg"
+}
---
+++ iOS/SnapShare/SnapShare/HttpHelper.swift
@@ -0,0 +1,14 @@
+//
+//  HttpHelper.swift
+//  SnapShare
+//
+//  Created by Alexander Akagi on 2/15/16.
+//  Copyright © 2016 Alexander Akagi. All rights reserved.
+//
+
+import Foundation
+
+struct HttpHelper {
+    static let API_AUTH_NAME = ApiKeys.test
+    
+}
