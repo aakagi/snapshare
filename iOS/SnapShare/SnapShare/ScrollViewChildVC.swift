5cca0682a3d93f22e3c99379d64ab754c9f47687
aakagi <aakagi@purdue.edu>
2016-02-21 22:28:37 -0800

    got a motherfuckin response from aws bitches - I attribute my success and great commit message to ballmers peak

--- iOS/SnapShare/SnapShare/ScrollViewChildVC.swift
+++ iOS/SnapShare/SnapShare/ScrollViewChildVC.swift
@@ -19,9 +19,9 @@ class ScrollViewChildVC: UIViewController, UINavigationBarDelegate {
     var fetchVideosParam: String?
     
 
-
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         
         // TODO - Make the nav bar more abstract
         
@@ -85,9 +85,84 @@ class ScrollViewChildVC: UIViewController, UINavigationBarDelegate {
     func test() {
         print("here")
         
-        
+        downloadImage("profile.jpg")
         
     }
+    
+    
+    func downloadImage(key: String){
+        
+        var completionHandler: AWSS3TransferUtilityDownloadCompletionHandlerBlock?
+        
+        //downloading image
+        
+        
+        let S3BucketName: String = "your_s3_bucketName"
+        let S3DownloadKeyName: String = key
+        
+        let expression = AWSS3TransferUtilityDownloadExpression()
+        expression.downloadProgress = {(task: AWSS3TransferUtilityTask, bytesSent: Int64, totalBytesSent: Int64, totalBytesExpectedToSend: Int64) in
+            dispatch_async(dispatch_get_main_queue(), {
+                let progress = Float(totalBytesSent) / Float(totalBytesExpectedToSend)
+                //self.progressView.progress = progress
+                //   self.statusLabel.text = "Downloading..."
+                NSLog("Progress is: %f",progress)
+            })
+        }
+        
+        
+        
+        completionHandler = { (task, location, data, error) -> Void in
+            dispatch_async(dispatch_get_main_queue(), {
+                if ((error) != nil){
+                    NSLog("Failed with error")
+                    NSLog("Error: %@",error!);
+                    //   self.statusLabel.text = "Failed"
+                }
+                    /*
+                    else if(self.progressView.progress != 1.0) {
+                    //    self.statusLabel.text = "Failed"
+                    NSLog("Error: Failed - Likely due to invalid region / filename")
+                    }   */
+                else{
+                    //    self.statusLabel.text = "Success"
+//                    self.collectionImages[S3DownloadKeyName] = UIImage(data: data!)
+                    //reload the collectionView data to include new picture
+//                    self.colView.reloadData()
+                    print("Success! - In completionHandler")
+                }
+            })
+        }
+        
+        let transferUtility = AWSS3TransferUtility.defaultS3TransferUtility()
+        
+        transferUtility.downloadToURL(nil, bucket: S3BucketName, key: S3DownloadKeyName, expression: expression, completionHander: completionHandler).continueWithBlock { (task) -> AnyObject! in
+            if let error = task.error {
+                NSLog("Error: %@",error.localizedDescription);
+                //  self.statusLabel.text = "Failed"
+            }
+            if let exception = task.exception {
+                NSLog("Exception: %@",exception.description);
+                //  self.statusLabel.text = "Failed"
+            }
+            if let _ = task.result {
+                //    self.statusLabel.text = "Starting Download"
+                //NSLog("Download Starting!")
+                // Do something with uploadTask.
+                /*
+                dispatch_async(dispatch_get_main_queue(), {
+                self.colView.reloadData()
+                })
+                */
+                
+                print(task.result)
+                
+            }
+            return nil;
+        }
+        
+    }
+    
     
     func navButtonClicked(sender: UIBarButtonItem) {
         // Goes right by default
